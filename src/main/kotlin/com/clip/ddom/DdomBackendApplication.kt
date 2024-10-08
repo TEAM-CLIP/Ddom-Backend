8f0df2046aca6acc7468a251a396e0a4173e1f9c
isprogrammingfun <wisjang1@naver.com>
2024-10-08 20:59:40 +0900

    Initial commit

---
+++ src/main/kotlin/com/clip/ddom/DdomBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.clip.ddom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DdomBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DdomBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/clip/ddom/DdomBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clip.ddom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DdomBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
