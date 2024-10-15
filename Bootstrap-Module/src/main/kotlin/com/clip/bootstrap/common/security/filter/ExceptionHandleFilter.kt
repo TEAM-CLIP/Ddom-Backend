fe6ec97cc024408c17e68a98660acd4f3688e868
isprogrammingfun <wisjang1@naver.com>
2024-10-15 23:52:54 +0900

    feat: API 요청 시 필터 단 예외처리 구현

---
+++ Bootstrap-Module/src/main/kotlin/com/clip/bootstrap/common/security/filter/ExceptionHandleFilter.kt
@@ -0,0 +1,59 @@
+package com.clip.bootstrap.common.security.filter
+
+import com.clip.bootstrap.common.exception.ExceptionResponse
+import com.clip.common.exception.BusinessException
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+@Order(0)
+class ExceptionHandleFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (e: Exception) {
+            handleException(e, response)
+        }
+    }
+
+    private fun handleException(e: Exception, response: HttpServletResponse) {
+        run {
+            when (e) {
+                is BusinessException -> {
+                    response.status = e.httpStatus
+                    response.outputStream.write(
+                        objectMapper.writeValueAsString(
+                            ExceptionResponse.of(e)
+                        ).toByteArray()
+                    )
+                    response.contentType = "application/json"
+                }
+
+                else -> {
+                    response.status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR
+                    response.outputStream.write(
+                        objectMapper.writeValueAsString(
+                            ExceptionResponse(
+                                message = e.message ?: "알 수 없는 오류가 발생했습니다.",
+                                code = "UNKNOWN-ERROR"
+                            )
+                        ).toByteArray()
+                    )
+                    response.contentType = "application/json"
+                }
+            }
+        }
+
+    }
+}
