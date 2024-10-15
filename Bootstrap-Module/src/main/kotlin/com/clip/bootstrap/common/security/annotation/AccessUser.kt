75a58a7c1b5f50eb8e7a6552bc055e67d2b5b212
isprogrammingfun <wisjang1@naver.com>
2024-10-15 23:49:21 +0900

    feat: JWT 검증 필터 추가, 사용자 검증 어노테이션 구현

---
+++ Bootstrap-Module/src/main/kotlin/com/clip/bootstrap/common/security/annotation/AccessUser.kt
@@ -0,0 +1,8 @@
+package com.clip.bootstrap.common.security.annotation
+
+import io.swagger.v3.oas.annotations.Parameter
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@Parameter(hidden = true)
+annotation class AccessUser()
