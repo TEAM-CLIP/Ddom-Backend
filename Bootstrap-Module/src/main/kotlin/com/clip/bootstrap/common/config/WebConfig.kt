3f449ff93a5d85a46029429b514e369f0f4ed448
isprogrammingfun <wisjang1@naver.com>
2024-10-15 23:36:36 +0900

    feat: Swagger 설정 및 Web 설정 파일 추가

---
+++ Bootstrap-Module/src/main/kotlin/com/clip/bootstrap/common/config/WebConfig.kt
@@ -0,0 +1,24 @@
+package com.clip.bootstrap.common.config
+
+import com.clip.bootstrap.common.security.annotation.AccessUserArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    private val accessUserArgumentResolver: AccessUserArgumentResolver
+) : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(accessUserArgumentResolver)
+    }
+}
