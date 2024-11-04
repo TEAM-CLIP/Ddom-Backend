e89dc9afbc7ab9f7023d6fa29de8b7152aa63473
isprogrammingfun <wisjang1@naver.com>
2024-11-05 00:08:33 +0900

    feat : 구역 도메인 클래스 구현

---
+++ Domain-Module/src/main/kotlin/com/clip/domain/store/entity/Zone.kt
@@ -0,0 +1,12 @@
+package com.clip.domain.store.entity
+
+import com.clip.domain.common.Aggregate
+import com.clip.domain.common.DomainId
+
+class Zone(
+    id: DomainId,
+    val name: String,
+    val description: String?
+): Aggregate<Zone>(id) {
+
+}
